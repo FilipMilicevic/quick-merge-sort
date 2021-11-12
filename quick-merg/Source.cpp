fe8845e8c4a781ec9ba0aa8defad0e027e5bb035
Filip Miličević <fmilic01@fesb.hr>
2021-11-12 15:05:24 +0100

    Add project files.

---
+++ quick-merg/Source.cpp
@@ -0,0 +1,135 @@
+﻿/* C implementation QuickSort */
+#include<stdio.h>
+#include<time.h>
+#include<iostream>
+#include<cstdio>
+
+const int kolicina = 100000;
+
+
+void swap(int* a, int* b)
+{
+	int t = *a;
+	*a = *b;
+	*b = t;
+}
+
+int partition(int arr[], int low, int high)
+{
+	int pivot = arr[high]; // pivot
+	int i = (low - 1); // Index of smaller element
+
+	for (int j = low; j <= high - 1; j++)
+	{
+		// If current element is smaller than or
+		// equal to pivot
+		if (arr[j] <= pivot)
+		{
+			i++; // increment index of smaller element
+			swap(&arr[i], &arr[j]);
+		}
+	}
+	swap(&arr[i + 1], &arr[high]);
+	return (i + 1);
+}
+
+void quickSort(int arr[], int low, int high)
+{
+	if (low < high)
+	{
+		/* pi is partitioning index, arr[p] is now
+		at right place */
+		int pi = partition(arr, low, high);
+
+		// Separately sort elements before
+		// partition and after partition
+		quickSort(arr, low, pi - 1);
+		quickSort(arr, pi + 1, high);
+	}
+}
+
+void merge(int array[], int const left, int const mid, int const right)
+{
+    auto const subArrayOne = mid - left + 1;
+    auto const subArrayTwo = right - mid;
+
+    // Create temp arrays
+    auto* leftArray = new int[subArrayOne],
+        * rightArray = new int[subArrayTwo];
+
+    // Copy data to temp arrays leftArray[] and rightArray[]
+    for (auto i = 0; i < subArrayOne; i++)
+        leftArray[i] = array[left + i];
+    for (auto j = 0; j < subArrayTwo; j++)
+        rightArray[j] = array[mid + 1 + j];
+
+    auto indexOfSubArrayOne = 0, // Initial index of first sub-array
+        indexOfSubArrayTwo = 0; // Initial index of second sub-array
+    int indexOfMergedArray = left; // Initial index of merged array
+
+    // Merge the temp arrays back into array[left..right]
+    while (indexOfSubArrayOne < subArrayOne && indexOfSubArrayTwo < subArrayTwo) {
+        if (leftArray[indexOfSubArrayOne] <= rightArray[indexOfSubArrayTwo]) {
+            array[indexOfMergedArray] = leftArray[indexOfSubArrayOne];
+            indexOfSubArrayOne++;
+        }
+        else {
+            array[indexOfMergedArray] = rightArray[indexOfSubArrayTwo];
+            indexOfSubArrayTwo++;
+        }
+        indexOfMergedArray++;
+    }
+    // Copy the remaining elements of
+    // left[], if there are any
+    while (indexOfSubArrayOne < subArrayOne) {
+        array[indexOfMergedArray] = leftArray[indexOfSubArrayOne];
+        indexOfSubArrayOne++;
+        indexOfMergedArray++;
+    }
+    // Copy the remaining elements of
+    // right[], if there are any
+    while (indexOfSubArrayTwo < subArrayTwo) {
+        array[indexOfMergedArray] = rightArray[indexOfSubArrayTwo];
+        indexOfSubArrayTwo++;
+        indexOfMergedArray++;
+    }
+}
+
+void mergeSort(int array[], int const begin, int const end)
+{
+    if (begin >= end)
+        return; // Returns recursively
+
+    auto mid = begin + (end - begin) / 2;
+    mergeSort(array, begin, mid);
+    mergeSort(array, mid + 1, end);
+    merge(array, begin, mid, end);
+}
+
+
+int main()
+{
+
+	int arr[kolicina];
+    int arr2[kolicina];
+
+	srand(time(NULL));
+	int i = 0;
+	while (i <kolicina)
+		arr[i++] = rand() % kolicina*10;
+
+    printf("Radi se sortiranje %d nasumicnih brojeva...\n", kolicina);
+    memcpy(arr2, arr, kolicina * sizeof(int));
+	clock_t elapsed2 = clock();
+	quickSort(arr, 0, kolicina - 1);
+	elapsed2 = clock() - elapsed2;
+	printf("Quick sort vrijeme je: %f sec\n", (float)elapsed2 / CLOCKS_PER_SEC);
+
+    clock_t elapsed = NULL;
+    elapsed = clock();
+    mergeSort(arr2,0,kolicina-1);
+    elapsed = clock() - elapsed;
+    printf("Merg sort vrijeme je: %f sec\n", (float)elapsed / CLOCKS_PER_SEC);
+    printf("\n \n \n \n \n \n \n");
+	return 0;
+}
